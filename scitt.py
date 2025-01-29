0f1045b971e9d5dcdfd76c86d2db4974485fad19
CatherineAtieno <kateatieno1@gmail.com>
2025-01-29 10:41:34 +0300

    this is the code for the number guessing game

--- scitt.py
+++ scitt.py
@@ -1,8 +1,27 @@
-"""  Please make sure you read the entire README.md file to follow the instructions. 
-You dont have to follow how I have started the game. Please use your creativity """
+import random
 
-print("Welcome to the Number Guessing Game!")
-pass
+def guess_the_number():
+    secret_number=random.randint(1,10)
+    attempts=0
+    print("welcome to the number guessing game")
+    print("I'm thinking of a number between 1 and 10.")
     
+    while True:
+        user_input=input("Enter your guess:")
 
+        if not user_input.isdigit():
+            print("Invalid input! Please enter a number.")
+            continue
+        guess=int(user_input)
+        attempts += 1
 
+        if guess< secret_number:
+            print ("Too low! Try again")
+        elif guess > secret_number:
+            print ("Too high! Try again")
+        else:
+          print (f"Congratulations! You have guessed the number in {attempts} attempts.")  
+          break
+        print ("thank you for playing the number guess game ")
+        
+guess_the_number()
